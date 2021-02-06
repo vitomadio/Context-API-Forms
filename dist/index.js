975a923e0cf72a823194878d6f00a60652d5e0a1
vitomadio <vitomadio@hotmail.com>
2021-02-06 16:15:40 +0100

    Add configuration for publish

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+import Form from '../Form';
+import Field from '../Field';
+import FieldArray from '../FieldArray';
+export { Form, Field, FieldArray };
---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+import Form from '../Form';
+import Field from '../Field';
+import FieldArray from '../FieldArray';
+
+export {
+    Form, 
+    Field,
+    FieldArray 
+}
