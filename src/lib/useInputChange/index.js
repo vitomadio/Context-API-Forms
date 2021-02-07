50e7aec150438702f0ac88556ad0aae619c6f647
vitomadio <vitomadio@hotmail.com>
2021-02-07 10:01:51 +0100

    Fix issue with FieldArray: was not adding new entries to de array

--- src/lib/useInputChange/index.js
+++ src/lib/useInputChange/index.js
@@ -2,7 +2,6 @@ import { useContext, useEffect } from "react";
 import { formStore } from "../store";
 
 const useInputChange = (ref, type, formName, fieldName, fieldArrayName) => {
-
   const { state, dispatch } = useContext(formStore);
   useEffect(() => {
     const inputChange = (event) => {
@@ -18,27 +17,39 @@ const useInputChange = (ref, type, formName, fieldName, fieldArrayName) => {
           value = event.target.checked;
           if (value !== true) {
             value = false;
-          } 
+          }
         }
         if (fieldArrayName) {
-          return dispatch({
+          if (state[formName] && state[formName][fieldArrayName] != null) {
+            console.log(state[formName][fieldArrayName]);
+            dispatch({
+              type: "change-form",
+              payload: {
+                [formName]: { [fieldArrayName]: { ...state[formName][fieldArrayName], [fieldName]: value } }
+              }
+            });
+          } else {
+            dispatch({
+              type: "change-form",
+              payload: {
+                [formName]: { [fieldArrayName]: { [fieldName]: value } }
+              }
+            }); 
+          }
+        } else {
+          dispatch({
             type: "change-form",
-            payload: {
-              [formName]: { [fieldArrayName]: { ...state[formName], [fieldName]: value } }
-            }
+            payload: { [formName]: { [fieldName]: value } }
           });
         }
-        return dispatch({
-          type: "change-form",
-          payload: { [formName]: { [fieldName]: value } }
-        });
       }
-    };
+    }
     document.addEventListener("change", inputChange);
     return () => {
       document.removeEventListener("change", inputChange);
     };
   }, [ref, dispatch, fieldArrayName, fieldName, formName, type, state]);
+  
 };
 
 export default useInputChange;
