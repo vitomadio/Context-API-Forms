90ce994fa4c0f7be945d170c94790e4b7159b4ba
vitomadio <vitomadio@hotmail.com>
2021-02-27 19:00:47 +0100

    Remove dist directory from github repo and ignore it for future commits

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import Form from './Form';
-import Field from './Field';
-import FieldArray from './FieldArray';
-import { formStore, FormProvider } from './store';
-export { Form, Field, FieldArray, FormProvider, formStore };
