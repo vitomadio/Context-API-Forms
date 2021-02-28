945087f0aaca6bf75d779853d556fe54d2f51063
vitomadio <vitomadio@hotmail.com>
2021-02-28 11:28:22 +0100

    Untrack dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import Form from './Form';
-import Field from './Field';
-import FieldArray from './FieldArray';
-import useSetInitialValues from './useSetInitialValues';
-import { formStore, FormProvider } from './store';
-export { Form, Field, FieldArray, FormProvider, formStore, useSetInitialValues };
