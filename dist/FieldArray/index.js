519a3a746dc1648a97bbe51808b7895b4c8d31d5
vitomadio <vitomadio@hotmail.com>
2021-02-28 11:21:58 +0100

    Modify Readme

---
+++ dist/FieldArray/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FieldArray = ({
+  formName,
+  fieldArrayName,
+  children
+}) => {
+  const childrenWithProps = React.Children.map(children, child => {
+    return /*#__PURE__*/React.cloneElement(child, {
+      fieldArrayName,
+      formName,
+      key: child.props.fieldName
+    });
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    name: fieldArrayName
+  }, childrenWithProps);
+};
+
+export default FieldArray;
---
+++ dist/useValidate/index.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useValidate = (ref, validations) => {
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    if (validations) {
+      const current = ref.current;
+
+      const validateInput = e => {
+        validations.map(validation => {
+          if (e.target.checked) {
+            return setError(validation(e.target.checked));
+          }
+
+          return setError(validation(e.target.value));
+        });
+      };
+
+      ref.current.addEventListener("focusout", validateInput);
+      return () => {
+        current.removeEventListener("focusout", validateInput);
+      };
+    }
+  }, [ref, validations]);
+  return error;
+};
+
+export default useValidate;
